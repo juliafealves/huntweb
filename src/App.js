c4e72fa750b715e12bf4e7f9d98ea85eb8fd8cd1
juliafealves <juliafealves>
2019-04-15 00:10:28 -0300

    Added Axios library, created main component and loading product by api.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
+import Header               from './components/Header';
+import Main                 from './pages/main';
 import './style.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/pages/main/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import api                  from '../../services/api';
+
+class Main extends Component {
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('products');
+        console.log(response);
+    };
+
+    render() {
+        return <h1>Hello!</h1>
+    }
+}
+
+export default Main;
