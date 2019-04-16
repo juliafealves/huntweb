2728df0925f31ae74fc3d77811beec4a6fd55984
juliafealves <juliafealves>
2019-04-16 00:10:20 -0300

    Created routes and show details about product.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header               from './components/Header';
-import Main                 from './pages/main';
+import Routes               from "./routes";
 import './style.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
         return (
             <div className="App">
                 <Header/>
-                <Main/>
+                <Routes/>
             </div>
         );
     }
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Link }             from 'react-router-dom';
 import api                  from '../../services/api';
-
 import './style.css'
 
 class Main extends Component {
@@ -15,10 +15,10 @@ class Main extends Component {
     }
 
     loadProducts = async (page = 1) => {
-        const response = await api.get(  `/products?page=${page}`);
+        const response = await api.get(`/products?page=${page}`);
         const { docs, ...productInfo } = response.data;
         this.setState({
-            products: response.data.docs,
+            products: docs,
             productInfo,
             page
         });
@@ -50,7 +50,7 @@ class Main extends Component {
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
 
-                        <a href="#">Acessar</a>
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
